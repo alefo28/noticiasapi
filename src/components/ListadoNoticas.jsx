ff708ffadd76fc112f025c675a288fbeb0dd714f
alefo28 <alefo28@gmail.com>
2023-09-07 09:53:18 -0600

    second commit

---
+++ src/components/ListadoNoticas.jsx
@@ -0,0 +1,51 @@
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import useNoticias from "../hooks/useNoticias";
+import Noticia from "./Noticia";
+
+const ListadoNoticas = () => {
+  const { noticias, totalNoticias, handleChangePagina, pagina } = useNoticias();
+
+  const totalPaginas = Math.ceil(totalNoticias / 20);
+
+  return (
+    <>
+      <Typography
+        textAlign={"center"}
+        marginY={5}
+        variant="h3"
+        component={"h2"}
+      >
+        Ultimas Noticias
+      </Typography>
+
+      <Grid container spacing={2}>
+        {noticias.map((noticia) => (
+          <>
+            {noticia.title !== "[Removed]" && (
+              <Noticia key={noticia.url} noticia={noticia} />
+            )}
+          </>
+        ))}
+      </Grid>
+      <Stack
+        sx={{ marginY: 5 }}
+        spacing={2}
+        direction={"row"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Pagination
+          count={totalPaginas}
+          color="primary"
+          onChange={handleChangePagina}
+          page={pagina}
+        />
+      </Stack>
+    </>
+  );
+};
+
+export default ListadoNoticas;
